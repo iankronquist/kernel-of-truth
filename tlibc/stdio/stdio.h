8c803530b37f85b21cf06b812bc768a07ec1a9fe
Ian Kronquist <iankronquist@teleport.com>
2014-02-01 19:13:53 -0800

    Remove attempted function overloads
    Last night I learned that you can't
    overload functions in C. That's ridiculous.

--- tlibc/stdio/stdio.h
+++ tlibc/stdio/stdio.h
@@ -3,15 +3,6 @@
 
 int printf(char* string, int value);
 
-//int printf(char* string, char value);
-
-// TODO: write these functions
-/*
-int printf(char* string, long value);
-int printf(char* string, double value);
-int printf(char* string, char* value);
-*/
-
 int puts(char* string);
 
 int putchar(char symbol);
