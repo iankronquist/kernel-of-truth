a3392e98feae5149c99363e5eb1ee01435a757ee
Ian Kronquist <iankronquist@teleport.com>
2014-02-02 23:39:00 -0800

    Fix bug in tmem.c
    Accidentally set the value of the fields in memset to `size`
    not `value`.

--- tlibc/tmem/mem.c
+++ tlibc/tmem/mem.c
@@ -35,7 +35,7 @@ void* memset(void* buffer, int value, size_t size)
 	unsigned char* bufferc = (unsigned char*) buffer;
 	for(size_t i = 0; i < size; i++)
 	{
-		bufferc[i] = (unsigned char) size;
+		bufferc[i] = (unsigned char) value;
 	}
 	return buffer;
 }
