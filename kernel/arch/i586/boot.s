9089df1a1e210145df4135c91d69c17364a53c89
Ian Kronquist <iankronquist@teleport.com>
2014-02-02 21:35:09 -0800

    Move GDT flush logic to external assembly file

--- kernel/arch/i586/boot.s
+++ kernel/arch/i586/boot.s
@@ -76,22 +76,4 @@ _start:
 # This is useful when debugging or when you implement call tracing.
 .size _start, . - _start
 
-# from http://www.osdever.net/bkerndev/Docs/gdt.htm
-# This will set up our new segment registers. We need to do
-# something special in order to set CS. We do what is called a
-# far jump. A jump that includes a segment as well as an offset.
-# This is declared in C as 'extern void gdt_flush();'
-.global _gdt_flush     # Allows the C code to link to this
-.extern _gp            # Says that '_gp' is in another file
-_gdt_flush:
-    lgdt [_gp]        # Load the GDT with our '_gp' which is a special pointer
-    mov ax, 0x10      # 0x10 is the offset in the GDT to our data segment
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    jmp 0x08:.flush2   # 0x08 is the offset to our code segment: Far jump!
-.flush2:
-    ret               # Returns back to the C code!
-
+#.file "start.asm"
