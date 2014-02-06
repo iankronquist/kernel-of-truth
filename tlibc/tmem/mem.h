76cab6bb3662c56950826baa28303d026f8ef162
Ian Kronquist <iankronquist@teleport.com>
2014-02-06 12:05:16 -0800

    Add header guards.

--- tlibc/tmem/mem.h
+++ tlibc/tmem/mem.h
@@ -1,6 +1,10 @@
+#ifndef TMEM_H
+#define TMEM_H
 #include <stddef.h>
 
 int memcmp(const void*, const void*, size_t);
 void* memcpy(void*, const void*, size_t);
 void* memmove(void*, const void*, size_t);
 void* memset(void*, int, size_t);
+
+#endif
