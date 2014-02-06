77172cfb1035c1e9a0bbb5f900107b3472b4c6dc
Ian Kronquist <iankronquist@teleport.com>
2014-02-06 12:06:34 -0800

    Split C files into separate header files.

---
+++ kernel/kabort.h
@@ -0,0 +1,7 @@
+#ifndef KABORT_H
+#define KABORT_H
+#include "kputs.h"
+
+void kabort();
+
+#endif
---
+++ kernel/kputs.h
@@ -0,0 +1,8 @@
+#ifndef KPUTS_H
+#define KPUTS_H
+
+#include "terminal.h"
+
+void kputs(char* string);
+
+#endif
