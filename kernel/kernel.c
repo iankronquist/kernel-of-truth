c6f7fc6df2bb40b1c9ff3870c4926f46f14591bc
Ian Kronquist <iankronquist@teleport.com>
2014-04-13 00:22:14 -0700

    Remove old implementation of interrupt tables

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,14 +1,12 @@
 #include <stddef.h>
 #include <stdint.h>
-#include "gdt.h"
-#include "idt.h"
-#include "isr.h"
 #include "terminal.h"
 #include "kabort.h"
 #include "kassert.h"
 
 
 /* Check if the compiler thinks if we are targeting the wrong operating system. */
+//Useful for possible new contributers.
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
@@ -21,12 +19,6 @@
  
 void kernel_main()
 {
-	gdt_install();
-	idt_install();
-	isrs_install();
 	term_initialize();
-	term_writestring("Interrupt?");
-	kassert(0 == 0);
-	isr0();
 	term_writestring("Hello, Kernel!");
 }
