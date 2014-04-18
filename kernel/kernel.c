12bca798f20e13490cd77a6092da75c23e4c7c00
Ian Kronquist <iankronquist@teleport.com>
2014-04-17 23:33:12 -0700

    kernel.c: add gdt to kernel

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,6 +3,7 @@
 #include "terminal.h"
 #include "kernel-functions/kabort.h"
 #include "kernel-functions/kassert.h"
+#include "arch/i586/gdt.h"
 
 
 /* Check if the compiler thinks if we are targeting the wrong operating system. */
@@ -21,4 +22,6 @@ void kernel_main()
 {
 	term_initialize();
 	term_writestring("Hello, Kernel!");
+    set_up_gdt();
+    term_writestring("And... success?");
 }
