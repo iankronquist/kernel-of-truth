dd76896de10bb2332f1e822c41bce45dbadd8d88
Ian Kronquist <iankronquist@teleport.com>
2015-11-05 12:47:15 -0800

    Refactor GDT

--- kernel/arch/x86/gdt.c
+++ kernel/arch/x86/gdt.c
@@ -1,24 +1,11 @@
 #include "gdt.h"
-/*GDT stands for "Global Descriptor Table". 
-It is a table available on i386 (ix86?)
-processors. The GDT is a list of 64 bit entries.
-GRUB (or our boot) loader will install the GDT for us.
-If we overwrite the GDT we will cause a 'triple fault'
-and the machine will be reset.
-
-Based on the tutorial, we will write a GDT with only 3 fields
-and will not concern ourselves with the other fields.
-
-The above was paraphrased from:
-http://www.osdever.net/bkerndev/Docs/gdt.htm
-*/
 
 typedef unsigned short ushort;
 typedef unsigned char uchar;
 typedef unsigned int uint;
 
-void gdt_set_gate(int num, unsigned long base, unsigned long limit,
-                  uchar access, uchar granularity)
+void gdt_set_gate(uint32_t num, uint64_t base, uint64_t limit,
+                  uint8_t access, uint8_t granularity)
 {
     //Set descriptor base access
     gdt[num].base_low = (base & 0xffff);
@@ -38,21 +25,19 @@ void gdt_install()
 {
     //Setup GDT pointer and limit
     gdtp.limit = (sizeof(struct gdt_entry) * 3) - 1;
-    gdtp.base = &gdt;
+    gdtp.base = (uint32_t) & gdt;
 
     // NULL descriptor
     gdt_set_gate(0, 0, 0, 0, 0);
 
     /* The second entry is our Code Segment. The base address
-	*  is 0, the limit is 4GBytes, it uses 4KByte granularity,
-	*  uses 32-bit opcodes, and is a Code Segment descriptor.
-	*  Please check the table above in the tutorial in order
-	*  to see exactly what each value means */
+    *  is 0, the limit is 4GBytes, it uses 4KByte granularity,
+    *  uses 32-bit opcodes, and is a Code Segment descriptor. */
     gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
 
     /* The third entry is our Data Segment. It's EXACTLY the
-	*  same as our code segment, but the descriptor type in
-	*  this entry's access byte says it's a Data Segment */
+    *  same as our code segment, but the descriptor type in
+    *  this entry's access byte says it's a Data Segment */
     gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
 
     /* Flush out the old GDT and install the new changes */
