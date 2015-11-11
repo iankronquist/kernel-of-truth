b896211141ebfdf0827dd4a6785f0f65be0810d7
Ian Kronquist <iankronquist@teleport.com>
2015-11-10 20:55:10 -0800

    Add key code to character translation table

--- include/drivers/keyboard.h
+++ include/drivers/keyboard.h
@@ -7,6 +7,9 @@
 #define KB_STATUS_PORT 0x64
 #define KB_DATA_PORT 0x60
 
+
+unsigned char keyboard_map[128];
+
 void keyboard_irq_handler();
 void keyboard_install();
 
--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -1,5 +1,47 @@
 #include <drivers/keyboard.h>
 
+// This table was shamelessly stolen from:
+// http://www.osdever.net/bkerndev/Docs/keyboard.htm
+unsigned char keyboard_map[128] = {
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',    /* 9 */
+    '9', '0', '-', '=', '\b',    /* Backspace */
+    '\t',            /* Tab */
+    'q', 'w', 'e', 'r',    /* 19 */
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',    /* Enter key */
+    0,            /* 29   - Control */
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',    /* 39 */
+    '\'', '`',   0,        /* Left shift */
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n',            /* 49 */
+    'm', ',', '.', '/',   0,                /* Right shift */
+    '*',
+    0,    /* Alt */
+    ' ',    /* Space bar */
+    0,    /* Caps lock */
+    0,    /* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,    /* < ... F10 */
+    0,    /* 69 - Num lock*/
+    0,    /* Scroll Lock */
+    0,    /* Home key */
+    0,    /* Up Arrow */
+    0,    /* Page Up */
+    '-',
+    0,    /* Left Arrow */
+    0,
+    0,    /* Right Arrow */
+    '+',
+    0,    /* 79 - End key*/
+    0,    /* Down Arrow */
+    0,    /* Page Down */
+    0,    /* Insert Key */
+    0,    /* Delete Key */
+    0,   0,   0,
+    0,    /* F11 Key */
+    0,    /* F12 Key */
+    0,    /* All other keys are undefined */
+};
+
+
 void keyboard_install() {
     // Enable only IRQ1
     write_port(0x21 , 0xFD);
@@ -22,7 +64,7 @@ void keyboard_irq_handler() {
         if (key_code < 0) {
             return;
         } else {
-            term_putchar(key_code);
+            term_putchar(keyboard_map[key_code]);
         }
     }
 
