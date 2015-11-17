49457bc9bc95c1d37fa5dc5d2e2ed481d9cb4a45
Ian Kronquist <iankronquist@teleport.com>
2015-11-17 11:10:14 -0800

    Create memlayout.h with kernel memory constants
    Define constants describing the kernel's location in memory. Inspired by xv6.
    Several of these constants MUST be the same as those in the linker.ld file!

---
+++ include/arch/x86/memlayout.h
@@ -0,0 +1,16 @@
+#ifndef MEMLAYOUT_H
+#define MEMLAYOUT_H
+
+#include <stdint.h>
+
+// 1 MB
+#define KERNEL_START 0x1000000
+
+// 4 KB
+#define KERNEL_SIZE (4096*4)
+
+#define VIRTUAL_TO_PHYSICAL(addr) ((uint32_t)(addr) - KERNEL_START)
+#define PHYSICAL_TO_VIRTUAL(addr) ((void *)(addr) + KERNEL_START)
+
+
+#endif
