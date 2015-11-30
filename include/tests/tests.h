631f2224078851badce199f1957c576a310787a3
Ian Kronquist <iankronquist@gmail.com>
2015-11-29 21:41:35 -0800

    Print diagnostics when tests fail
    Take advantage of C11's macro generics system.

--- include/tests/tests.h
+++ include/tests/tests.h
@@ -9,45 +9,64 @@
 
 int RETURN_VALUE = 0;
 
-#define EXPECT_EQ(a, b) printf("Test %s == %s ", (#a), (#b));\
+#define PRINTF_FORMAT(x) _Generic(x, int: "%i", long: "%l", \
+        unsigned int: "%iu", unsigned long: "%lu", char*: "%s", void*: "%p", \
+        struct kheap_metadata*: "%p", default: "Unknown type!")
+
+#define DIAGNOSTICS(a, b) \
+        printf("Expected "); \
+        printf("%s: ", #a); \
+        printf(PRINTF_FORMAT(a), (a)); \
+        printf("\n");\
+        printf("Actual "); \
+        printf("%s: ", #b); \
+        printf(PRINTF_FORMAT(b), (b)); \
+        printf("\n");
+
+
+#define EXPECT_EQ(a, b); printf("Test %s == %s ", (#a), (#b));\
 if ((a) == (b)) {\
     printf(" %sPassed%s\n", GREEN, NOCOLOR);\
 }\
 else {\
     printf(" %sFailed%s\n", RED, NOCOLOR);\
+    DIAGNOSTICS(a, b); \
     RETURN_VALUE = EXIT_FAILURE;\
 }
 
-#define EXPECT_EQ_STR(a, b) printf("Test %s == %s ", (#a), (#b));\
+#define EXPECT_EQ_STR(a, b); printf("Test %s == %s ", (#a), (#b));\
 if (strcmp((a), (b)) == 0) {\
     printf(" %sPassed%s\n", GREEN, NOCOLOR);\
 }\
 else {\
     printf(" %sFailed%s\n", RED, NOCOLOR);\
+    DIAGNOSTICS(a, b); \
     RETURN_VALUE = EXIT_FAILURE;\
 }
 
-#define EXPECT_NEQ_STR(a, b) printf("Test %s != %s ", (#a), (#b));\
+#define EXPECT_NEQ_STR(a, b); printf("Test %s != %s ", (#a), (#b));\
 if (strcmp((a), (b)) != 0) {\
     printf(" %sPassed%s\n", GREEN, NOCOLOR);\
 }\
 else {\
     printf(" %sFailed%s\n", RED, NOCOLOR);\
+    DIAGNOSTICS(a, b); \
     RETURN_VALUE = EXIT_FAILURE;\
 }
 
-#define EXPECT_NEQ(a, b) printf("Test %s != %s ", (#a), (#b));\
+#define EXPECT_NEQ(a, b); printf("Test %s != %s ", (#a), (#b));\
 if ((a) != (b)) {\
     printf(" %sPassed%s\n", GREEN, NOCOLOR);\
 }\
 else {\
     printf(" %sFailed%s\n", RED, NOCOLOR);\
+    DIAGNOSTICS(a, b); \
     RETURN_VALUE = EXIT_FAILURE;\
 }
 
 #define EXPECT_CONTAINS(value, sequence, range)\
 printf("Test %s in %s ", (#value), (#sequence));\
-int passed = 0;\
+passed = 0;\
 for (size_t i = 0; i < range; i++) {\
     if (sequence[i] == value) {\
         passed = 1;\
@@ -57,16 +76,18 @@ for (size_t i = 0; i < range; i++) {\
 }\
 if (!passed) {\
     printf("%sFailed%s\n", RED, NOCOLOR);\
+    DIAGNOSTICS(a, b); \
     RETURN_VALUE = EXIT_FAILURE;\
 }
 
 #define EXPECT_NCONTAINS(value, sequence, range)\
 printf("Test %s in %s ", (#value), (#sequence));\
-int failed = 0;\
+failed = 0;\
 for (size_t i = 0; i < range; i++) {\
     if (sequence[i] == value) {\
         failed = 1;\
         printf("%sFailed%s\n", RED, NOCOLOR);\
+        DIAGNOSTICS(a, b); \
         RETURN_VALUE = EXIT_FAILURE;\
     }\
 }\
