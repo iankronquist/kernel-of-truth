fb0da0b3b4ece34207375ecddd022f2e2d5b7c52
Ian Kronquist <iankronquist@gmail.com>
2015-12-06 22:02:44 -0800

    Update headers to match new printf

--- include/libk/kputs.h
+++ include/libk/kputs.h
@@ -1,8 +1,9 @@
 #ifndef KPUTS_H
 #define KPUTS_H
+#include <stdarg.h>
 #include "drivers/terminal.h"
 
 void kputs(char* string);
-void kprint_int(char* string, int i);
+void kprintf(char* string, ...);
 
 #endif
