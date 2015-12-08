2d7c92e964ee88c9dc55270218f11acdf8ef9d0b
Ian Kronquist <iankronquist@gmail.com>
2015-12-07 22:13:05 -0800

    Update testing stubs to map kprintf to vfprintf

--- kernel/libk/tests/stubs.c
+++ kernel/libk/tests/stubs.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include <assert.h>
 
 
 void kputs(char* string) {
     puts(string);
+}
+
+void kprintf(char* string, ...) {
+    va_list a;
+    va_start(a, string);
+    vfprintf(stdout, string, a);
 }
 
 void kabort() {
