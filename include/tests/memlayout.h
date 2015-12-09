8750af15aab8a4c4e4673ce3aac33a81401d35e0
Ian Kronquist <iankronquist@gmail.com>
2015-12-08 22:16:24 -0800

    Add a version of memlayout for tests

---
+++ include/tests/memlayout.h
@@ -0,0 +1,15 @@
+#ifndef MEMLAYOUT_H
+#define MEMLAYOUT_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+// Make the number of entries 1024 for the physical_allocator test
+#define PHYS_MEMORY_SIZE 8*1024*4096
+#define PAGE_SIZE 4096
+uintptr_t dependencies_suck;
+#define PLAYGROUND_SIZE (PAGE_SIZE * 5)
+#define KHEAP_PHYS_END (void*)(dependencies_suck+PLAYGROUND_SIZE)
+#define KERNEL_END 0
+
+#endif
