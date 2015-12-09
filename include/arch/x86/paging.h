6ba1f5a4eceb4f0d601c388003de27b2414727ea
Ian Kronquist <iankronquist@gmail.com>
2015-12-08 22:18:06 -0800

    Add real paging
    Use fractal paging to get around thorny allocations!
    Add a map_page function.

--- include/arch/x86/paging.h
+++ include/arch/x86/paging.h
@@ -8,11 +8,11 @@
 #include <libk/kassert.h>
 #include <libk/kabort.h>
 #include <libk/kmem.h>
+#include <libk/physical_allocator.h>
 
 #include <arch/x86/memlayout.h>
 
 #define PAGE_TABLE_SIZE 1024
-#define PAGE_SIZE 4096
 
 // The macros PAGE_DIRECTORY, PAGE_ALIGN, and NEXT_PAGE are defined in
 // memlayout.h
@@ -21,10 +21,17 @@
 #define TOP10(x) ((uintptr_t)(x) & 0xffc00000)
 #define MID10(x) ((uintptr_t)(x) & 0x003ff000)
 #define LOW10(x) ((uintptr_t)(x) & 0x000003ff)
+
+#define HIGHINDEX(x) ((uintptr_t)x >> 22)
+#define LOWINDEX(x) ((uintptr_t)x >> 12)
+#define GETADDRESS(x) ((uintptr_t)x & ~ 0xfff)
 #define PAGE_TABLE_SIZE 1024
+
+#define PAGE_PRESENT 1
 
 void kernel_page_table_install();
 
 extern void enable_paging(uint32_t *page_dir);
-
+int map_page(page_frame_t physical_page, void *virtual_address,
+        uint16_t permissions);
 #endif
--- kernel/arch/x86/paging.c
+++ kernel/arch/x86/paging.c
@@ -1,14 +1,66 @@
 #include <arch/x86/paging.h>
 
 void kernel_page_table_install() {
-    uint32_t *page_dir = (uint32_t*)PAGE_DIRECTORY;
-    uint32_t *cur_page_entry = (uint32_t*)PAGE_DIRECTORY + PAGE_SIZE;
-    for (uint32_t table_i = 0; table_i < PAGE_TABLE_SIZE; table_i++) {
-        page_dir[table_i] = TOP20(cur_page_entry) | 1;
-        for (uint32_t entry_i = 0; entry_i < PAGE_TABLE_SIZE; entry_i++) {
-            cur_page_entry[entry_i] = LOW10(table_i) << 22 | LOW10(entry_i) << 12 | 1;
-        }
-        cur_page_entry += PAGE_SIZE;
+    // Certain very important things already exist in physical memory. They
+    // need to be marked as present so that the allocator doesn't grab them by
+    // accident.
+    // After they are marked as present they can safely be mapped with
+    // map_page.
+
+    // Mark all the pages the kernel sits on as in use
+    for (page_frame_t i = KERNEL_START; i < KERNEL_END; i += PAGE_SIZE) {
+        use_frame(i);
     }
+    // Mark the kernel heap as in use
+    use_frame(KHEAP_PHYS_ROOT);
+    // Mark video memory as in use
+    use_frame(VIDEO_MEMORY_BEGIN);
+    // Mark the paging directory as in use
+    use_frame(PAGE_DIRECTORY);
+
+    uint32_t *page_dir = (uint32_t*)PAGE_DIRECTORY;
+    memset(page_dir, 0, PAGE_TABLE_SIZE);
+
+    // Fractal page mapping
+    page_dir[PAGE_TABLE_SIZE-1] |= GETADDRESS(page_dir) | PAGE_PRESENT;
+
+    // Map all those important bits
+    for (page_frame_t i = KERNEL_START; i < KERNEL_END; i += PAGE_SIZE) {
+        map_page(i, (void*)i, 0);
+    }
+    map_page(KHEAP_PHYS_ROOT, (void*)KHEAP_PHYS_ROOT, 0);
+    map_page(VIDEO_MEMORY_BEGIN, (void*)VIDEO_MEMORY_BEGIN, 0);
+    map_page(PAGE_DIRECTORY, (void*)PAGE_DIRECTORY, 0);
+
     enable_paging(page_dir);
+}
+
+int map_page(page_frame_t physical_page, void *virtual_address,
+        uint16_t permissions) {
+
+    uint32_t *page_entry;
+    uint32_t *page_dir = (uint32_t*)PAGE_DIRECTORY;
+    uint32_t dir_index = HIGHINDEX(virtual_address);
+    uint32_t entry_index = LOWINDEX(virtual_address);
+
+    // Be sure.
+    kassert(((uintptr_t)physical_page % PAGE_SIZE) == 0);
+    kassert(((uintptr_t)virtual_address % PAGE_SIZE) == 0);
+
+
+    // If the relevant page directory entry is absent, allocate it.
+    if ((page_dir[dir_index] & 1) == 0) {
+        page_entry = (uint32_t*)alloc_frame();
+
+        page_dir[HIGHINDEX(page_entry)] =
+            GETADDRESS(page_entry) | permissions | PAGE_PRESENT;
+
+        page_dir[dir_index] = GETADDRESS(page_entry) | permissions |
+            PAGE_PRESENT;
+    } else {
+        page_entry = (uint32_t*)GETADDRESS(page_dir[dir_index]);
+    }
+    page_entry[entry_index] |= GETADDRESS(virtual_address) | permissions |
+        PAGE_PRESENT;
+    return 0;
 }
