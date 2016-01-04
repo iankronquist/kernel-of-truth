7968763bbf52551bf8c43b685e217a48ee67c4d9
Ian Kronquist <iankronquist@gmail.com>
2016-01-03 17:44:16 -0800

    Reload segment registers after installing the GDT
    Also convert everything to Intel syntax.

--- kernel/arch/x86/gdt.s
+++ kernel/arch/x86/gdt.s
@@ -1,18 +1,21 @@
+.intel_syntax noprefix
 .global gdt_flush
 .extern gdtp
 
 gdt_flush:
 	lgdt gdtp        # Load the GDT with our '_gp' which is a special pointer
+	jmp reload_code_segment
 	ret
 
+.global reload_code_segment
 reload_code_segment:
-	jmp 0x08+flush2
+	jmp 0x08:flush2
 flush2:
-	mov %ax, 0x10      # 0x10 is the offset in the GDT to our data segment
-	mov %ds, %ax
-	mov %es, %ax
-	mov %fs, %ax
-	mov %gs, %ax
-	mov %ss, %ax
+	mov ax, 0x10      # 0x10 is the offset in the GDT to our data segment
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
 	#jump to an 0x08 offset from flush2.
 	ret
