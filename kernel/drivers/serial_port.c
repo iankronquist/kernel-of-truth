94ba539c6e39beae2f963833cacbc70af4020355
Ian Kronquist <iankronquist@gmail.com>
2016-01-04 00:35:54 -0800

    Refactor serial port driver
    1. Use read and write port functions instead of inline assembly
    2. Remove unused macros
    3. Don't enable interrupts we don't use

--- kernel/drivers/serial_port.c
+++ kernel/drivers/serial_port.c
@@ -2,71 +2,42 @@
 #include <arch/x86/io.h>
 #include <arch/x86/idt.h>
 
-#define SERIAL_1 0x3f8
-#define SERIAL_2 0x2f8
-
-// Constants for the Received Data Status Register
-#define DATA_CARRIER_DETECT (1<<7)
-#define CLEAR_TO_SEND (1<<6)
-#define DATA_SET_READY (1<<5)
-#define BREAK (1<<4)
-#define FRAMING (1<<3)
-#define PARITY_ERROR (1<<2)
-#define OVERRUN_ERROR (1<<1)
-#define ERROR_OR_BREAK (1<<0)
-
-// Serial Port Compatible Register Address Offsets
-#define TRANSMIT_HOLDING(x) (x+0)
-#define RECEIVER_BUFFER(x) (x+0)
-#define DIVISOR_LATCH_LOW(x) (x+0)
-#define DIVISOR_LATCH_HIGH(x) (x+1)
-#define INTERRUPT_ENABLE(x) (x+1)
-#define INTERRUPT_ID(x) (x+2)
-#define FIFO_CONTROL(x) (x+2)
-#define LINE_CONTROL(x) (x+3)
-#define MODEM_CONTROL(x) (x+4)
-#define LINE_STATUS(x) (x+5)
-#define MODUM_STATUS(x) (x+6)
-#define SCRATCH(x) (x+7)
-
-void outportb(unsigned short _port, unsigned char _data) {
-	__asm__ volatile ("outb %1, %0" : : "dn" (_port), "a" (_data));
-}
-
-unsigned char inportb(unsigned short _port) {
-	unsigned char rv;
-	__asm__ volatile ("inb %1, %0" : "=a" (rv) : "dn" (_port));
-	return rv;
-}
-
-
 void initialize_serial_port(int port) {
-    outportb(port + 1, 0x00); /* Disable interrupts */
-    outportb(port + 3, 0x80); /* Enable divisor mode */
-    outportb(port + 0, 0x01); /* Div Low:  01 Set the port to 115200 bps */
-    outportb(port + 1, 0x00); /* Div High: 00 */
-    outportb(port + 3, 0x03); /* Disable divisor mode, set parity */
-    outportb(port + 2, 0xC7); /* Enable FIFO and clear */
-    outportb(port + 4, 0x0B); /* Enable interrupts */
-    outportb(port + 1, 0x01); /* Enable interrupts */
+    // Disable interrupts
+    write_port(port + 1, 0x00);
+    // Enable divisor mode to set clock rate
+    write_port(port + 3, 0x80);
+    // Set low bytes of divisor to 115200 baud
+    write_port(port + 0, 0x01);
+    // Set high bytes of divisor
+    write_port(port + 1, 0x00);
+    // Disable divisor mode and set parity
+    write_port(port + 3, 0x03);
+    // Enable FIFO mode and clear buffer
+    write_port(port + 2, 0xC7);
+    // Enable interrupts
+    /*
+    write_port(port + 4, 0x0B);
+    write_port(port + 1, 0x01);
+    */
 }
 
 int serial_received(int port) {
-    return inportb(port) & 1;
+    return read_port(port) & 1;
 }
 
 char read_serial(int port) {
     while (serial_received(port) == 0);
-    return inportb(port);
+    return read_port(port);
 }
 
 bool is_transmit_empty(int port) {
-    return inportb(port + 5) & 0x20;
+    return read_port(port + 5) & 0x20;
 }
 
 void write_serial(int port, char data) {
     while (is_transmit_empty(port) == 0);
-    outportb(port, data);
+    write_port(port, data);
 }
 
 void write_serial_string(int port, char *str) {
