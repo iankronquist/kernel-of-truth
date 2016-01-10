7e50d821503cd663f83b8590aafd6d998dc5d090
Ian Kronquist <iankronquist@gmail.com>
2016-01-10 02:00:52 -0800

    Remove unused header

--- include/arch/x86/cpu_state.h
+++
@@ -1,9 +0,0 @@
-#ifndef CPU_STATE_H
-#define CPU_STATE_H
-
-struct cpu_state {
-    // Pushed by pusha
-    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-    uint32_t eip, cs, eflags, useresp, ss;
-};
-#endif
