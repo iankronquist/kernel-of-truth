2322914c17e8e39b9893c22ab457031b60ae1dfd
Rasmus Larsen <doctorherbstein@gmail.com>
2016-06-17 20:33:58 +0200

    Updated GCC build scripts to use a modern GCC version

--- scripts/build_cross_compiler.sh
+++ scripts/build_cross_compiler.sh
@@ -23,31 +23,31 @@ if [[ ! -d compiler/src ]]; then
 
 	echo 'Downloading sources'
 	echo $(pwd)
-	wget http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz 
-	wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.9.1/gcc-4.9.1.tar.gz
-	wget https://gmplib.org/download/gmp/gmp-6.0.0a.tar.xz
-	wget http://www.mpfr.org/mpfr-3.1.2/mpfr-3.1.2.tar.xz
-	wget ftp://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz
+	wget http://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz
+	wget http://ftp.gnu.org/gnu/gcc/gcc-6.1.0/gcc-6.1.0.tar.gz
+	wget http://ftp.gnu.org/gnu/gmp/gmp-6.1.0.tar.xz
+	wget http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.4.tar.xz
+	wget http://ftp.gnu.org/gnu/mpc/mpc-1.0.3.tar.gz
 	if [ "$(uname)" == "Darwin" ]; then
 		wget http://www.bastoul.net/cloog/pages/download/count.php3?url=./cloog-parma-0.16.1.tar.gz
-		wget http://isl.gforge.inria.fr/isl-0.13.tar.xz
+		wget http://isl.gforge.inria.fr/isl-0.17.tar.xz
 	fi
 
 	echo 'Extracting archives'
 	echo $(pwd)
-	tar xzf binutils-2.24.tar.gz
-	tar xzf gcc-4.9.1.tar.gz
-	tar xzf mpc-1.0.2.tar.gz
-	tar xJf gmp-6.0.0a.tar.xz
-	tar xJf mpfr-3.1.2.tar.xz
-	mv gmp-6.0.0a gcc-4.9.1/gmp
-	mv mpfr-3.1.2 gcc-4.9.1/mpfr
-	mv mpc-1.0.2 gcc-4.9.1/mpc
+	tar xzf binutils-2.26.tar.gz
+	tar xzf gcc-6.1.0.tar.gz
+	tar xzf mpc-1.0.3.tar.gz
+	tar xJf gmp-6.1.0.tar.xz
+	tar xJf mpfr-3.1.4.tar.xz
+	mv gmp-6.1.0 gcc-6.1.0/gmp
+	mv mpfr-3.1.4 gcc-6.1.0/mpfr
+	mv mpc-1.0.3 gcc-6.1.0/mpc
 	if [ "$(uname)" == "Darwin" ]; then
 		tar xzf cloog-parma-0.16.1.tar.gz
-		tar xJf isl-0.13.tar.xz
-		mv cloog-parma-0.16.1 gcc-4.9.1/cloog
-		mv isl-0.13 gcc-4.9.1/isl
+		tar xJf isl-0.17.tar.xz
+		mv cloog-parma-0.16.1 gcc-6.1.0/cloog
+		mv isl-0.17 gcc-6.1.0/isl
 	fi
 	cd -
 fi
@@ -56,7 +56,7 @@ echo 'Building binutils'
 echo $(pwd)
 mkdir compiler/build/binutils
 cd compiler/build/binutils
-../../src/binutils-2.24/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+../../src/binutils-2.26/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make
 make install
 cd -
@@ -65,7 +65,7 @@ echo 'Building gcc'
 echo $(pwd)
 mkdir compiler/build/gcc
 cd compiler/build/gcc
-../../src/gcc-4.9.1/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+../../src/gcc-6.1.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
 make all-target-libgcc
 make install-gcc
