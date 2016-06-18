5fcd410c2abe6a2a3a59472bb51b4cdadbd79d47
Ian Kronquist <iankronquist@gmail.com>
2016-06-17 19:07:36 -0700

    Document and refactor the GDT

--- include/arch/x86/gdt.h
+++ include/arch/x86/gdt.h
@@ -3,12 +3,34 @@
 
 #include <stdint.h>
 
-extern void gdt_flush();
-
-/* the __attribute__((packed))
-tells the compiler not to 'optimize' the struct for us by 
-packing.
-*/
+/* The Global Descriptor Table and its entries.
+ * x86 uses a backward, antiquated segmented memory model for compatibility
+ * with computers which were around in the late Cretaceous. The Global
+ * Descriptor Table, or <gdt>, can control whether or not certain sections of
+ * memory can run with elevated privileges (ring 0), as well as whether they
+ * are used for code or data. Additionally, one 'gate', or entry in the GDT
+ * called the Task Struct Segment or TSS, can be used to set up hardware
+ * multitasking. We follow the example of professional OSs like Linux as well
+ * as hobbyist systems like ToaruOS and do our best to ignore both the GDT and
+ * hardware multitasking as much as possible. Better mechanisms exist for
+ * enforcing access control, such as paging. Additionally, I read that properly
+ * implemented software multitasking is faster than hardware multitasking
+ * anyway. Nonetheless, we must placate the Gods of x86 and create a GDT and a
+ * TSS.
+ *
+ * Our GDT will have 6 entries:
+ *
+ * 1. An empty entry because the manual says so.
+ * 2. A kernel mode code segment.
+ * 3. A kernel mode data segment.
+ * 4. A user mode code segment (not yet created).
+ * 5. A user mode data segment (not yet created).
+ * 6. A TSS entry (not yet created).
+ *
+ * This structure represents a single entry in the <gdt>. See figure 3-8,
+ * Chapter 3, Volume 3 of the Intel Manual and the rest of the chapter for more
+ * information.
+ */
 struct gdt_entry {
     uint16_t limit_low;
     uint16_t base_low;
@@ -16,25 +38,26 @@ struct gdt_entry {
     uint8_t access;
     uint8_t granularity;
     uint8_t base_high;
-} __attribute__((packed));
+}__attribute__((packed)) gdt[3];
 
-/* the following is a special pointer representing
-the max bytes taken up by the GDT -1 */
+/* A special pointer representing a pointer to the <gdt> and its size.
+ * For whatever reason, we only record the full size minus 1.
+ */
 struct gdt_ptr {
     uint16_t limit;
     uint32_t base;
-} __attribute__((packed));
+}__attribute__((packed)) gdtp;
 
-struct gdt_entry gdt[3];
-struct gdt_ptr gdtp;
-
-/* A function in start.am. Used to properly 
-reload the new segment registers */
-//extern void gdt_flush();
-
+/* Properly initialize an entry in the <gdt> */
 void gdt_set_gate(uint32_t num, uint64_t base, uint64_t limit,
                   uint8_t access, uint8_t granularity);
 
-void gdt_install();
+/* An assembly function which loads the <gdt>.
+ * It is a simple wrapper around the **lgdt** instruction.
+ */
+extern void gdt_flush(void);
+
+/* Install the <gdt> and set it up as described. */
+void gdt_install(void);
 
 #endif
--- kernel/arch/x86/gdt.c
+++ kernel/arch/x86/gdt.c
@@ -10,7 +10,7 @@ void gdt_set_gate(uint32_t num, uint64_t base, uint64_t limit,
 
     //Set descriptor limits
     gdt[num].limit_low = (limit & 0xffff);
-    gdt[num].granularity = ((limit >> 16) & 0xf0);
+    gdt[num].granularity = ((limit >> 16) & 0x0f);
 
     //Set granularity flags
     gdt[num].granularity |= (granularity & 0xf0);
@@ -26,13 +26,13 @@ void gdt_install()
     // NULL descriptor
     gdt_set_gate(0, 0, 0, 0, 0);
 
-    /* The second entry is our Code Segment. The base address
-    *  is 0, the limit is 4GBytes, it uses 4KByte granularity,
+    /* The second entry is the Code Segment. The base address
+    *  is 0, the limit is 4GB, it uses 4KB granularity,
     *  uses 32-bit opcodes, and is a Code Segment descriptor. */
     gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
 
-    /* The third entry is our Data Segment. It's EXACTLY the
-    *  same as our code segment, but the descriptor type in
+    /* The third entry is the Data Segment. It's exactly the
+    *  same as the code segment, but the descriptor type in
     *  this entry's access byte says it's a Data Segment */
     gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
 
