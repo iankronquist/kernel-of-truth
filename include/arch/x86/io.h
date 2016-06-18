ff65dc6c70927bc15fc6a06cf02644331d5838bc
Ian Kronquist <iankronquist@gmail.com>
2016-06-17 21:48:59 -0700

    Document the I/O functions

--- include/arch/x86/io.h
+++ include/arch/x86/io.h
@@ -2,8 +2,14 @@
 #define IO_H
 #include <stdint.h>
 
+/* Write to an I/O port.
+ * A wrapper around outb. Takes a @value to write and a @port to write it to.
+ */
 void write_port(uint16_t value, uint16_t port);
 
+/* Read from an I/O port.
+ * A wrapper around inb. Takes a @port and returns its value.
+ */
 uint16_t read_port(uint16_t port);
 
 #endif
