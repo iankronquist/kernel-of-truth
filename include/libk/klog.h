c93f7cdd22bfe85e0dbe6c9cac77399be903ad44
Ian Kronquist <iankronquist@gmail.com>
2016-06-18 16:44:59 -0700

    Document klog family of functions in headers

--- include/libk/klog.h
+++ include/libk/klog.h
@@ -7,8 +7,13 @@
 
 #include <drivers/serial_port.h>
 
+/* Initialize the kernel logging system.
+ * Currently logs to the COM1 serial port.
+ */
 void initialize_klog();
+/* Log a single string message. */
 void klog(char *message);
+/* Log a printf-style formatted message */
 void klogf(char* string, ...);
 
 #endif
