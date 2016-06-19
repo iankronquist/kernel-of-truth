ff958f17cb142ccdeb64125754c6bbef49760b85
Ian Kronquist <iankronquist@gmail.com>
2016-06-18 17:19:06 -0700

    Document and refactor the serial port driver

--- kernel/drivers/serial_port.c
+++ kernel/drivers/serial_port.c
@@ -2,7 +2,7 @@
 #include <arch/x86/io.h>
 #include <arch/x86/idt.h>
 
-void initialize_serial_port(int port) {
+void initialize_serial_port(enum COM_PORT port) {
     // Disable interrupts
     write_port(port + 1, 0x00);
     // Enable divisor mode to set clock rate
@@ -20,29 +20,34 @@ void initialize_serial_port(int port) {
     write_port(port + 1, 0x01);
 }
 
-int serial_received(int port) {
+static inline bool serial_received(enum COM_PORT port) {
     return read_port(port) & 1;
 }
 
-char read_serial(int port) {
+char read_serial(enum COM_PORT port) {
     while (serial_received(port) == 0);
     return read_port(port);
 }
 
-bool is_transmit_empty(int port) {
+static inline bool is_transmit_empty(enum COM_PORT port) {
     return read_port(port + 5) & 0x20;
 }
 
-void write_serial(int port, char data) {
+void write_serial(enum COM_PORT port, char data) {
     while (is_transmit_empty(port) == 0);
     write_port(port, data);
 }
 
-// We spend an awful lot of time in this function, so let's inline things to
-// speed it up a little.
-void write_serial_string(int port, char *str) {
+void write_serial_string(enum COM_PORT port, char *str) {
     for (size_t i = 0; str[i] != '\0'; ++i) {
-        while ((read_port(port + 5) & 0x20) == 0);
+        while ((is_transmit_empty(port)) == 0);
         write_port(port, str[i]);
+    }
+}
+
+void read_serial_string(enum COM_PORT port, char *buf, size_t len) {
+    for (size_t i = 0; i < len; ++i) {
+        while (serial_received(port) == 0);
+        buf[i] = read_serial(port);
     }
 }
