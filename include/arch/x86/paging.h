440286e28eed77928b95dde7b6ee1f8168cf2956
Ian Kronquist <iankronquist@gmail.com>
2016-07-14 19:44:18 -0400

    Document paging code

--- include/arch/x86/paging.h
+++ include/arch/x86/paging.h
@@ -44,29 +44,51 @@
 #define CUR_PAGE_DIRECTORY_ADDR ((uint32_t*)(0xfffff000))
 #define PAGING_DIR_PHYS_ADDR 0xffc00000
 
+// Set the current page table to the provided top level directory and enable
+// paging.
 extern void enable_paging(page_frame_t page_dir);
+// Disable paging.
 extern void disable_paging(void);
+// Do not change the page table, just flip the paging bit.
 extern void just_enable_paging(void);
+// Get the current page table directory.
 extern page_frame_t get_page_dir(void);
+// Flush the translation look-aside buffer.
 extern void flush_tlb(void);
 
+// Install the kernel page table.
 page_frame_t kernel_page_table_install(struct multiboot_info*);
 
+// Create a new page directory and map important data.
+// Map code at entrypoint, the code location, and the code's stack.
 page_frame_t create_page_dir(void *link_loc, void *stack_loc,
         void(*entrypoint)(), uint16_t permissions);
+// Find an unused physical address and map it.
+// Find a free virtual address in the paging directory, and map the provided
+// physical address with the given permissions.
 void *find_free_addr(uint32_t *page_dir, page_frame_t phys_addr,
         uint16_t permissions);
 
+// Identity map kernel.
 void map_kernel_pages(uint32_t *page_dir);
 
+// Map a page when paging is disabled.
+// Map a physical page in the page directory at the given virtual address with
+// permissions.
 int map_page(uint32_t *page_dir, page_frame_t physical_page,
         void *virtual_address, uint16_t permissions);
 
+// Unmap a page from a different page directory than the current one.
+// if should_free_frame is true, the resident page frame will be marked as
+// free.
 int inner_unmap_page(uint32_t *page_entries, void *virtual_address,
         bool should_free_frame);
 
+// Free a page table.
 void free_table(uint32_t *page_dir);
+// Map the kernel pages into a page table different than the current one.
 void inner_map_kernel_pages(uint32_t *page_dir);
+// Map a page into a different page table than the current one.
 int inner_map_page(uint32_t *page_dir, page_frame_t physical_page,
         void *virtual_address, uint16_t permissions);
 #endif
