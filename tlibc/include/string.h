2c906f71e8e9d1f9c7862e5f6833fc861d8eb15d
Ian Kronquist <iankronquist@gmail.com>
2016-07-17 20:10:53 -0400

    Add strnlen an strncpy to tlibc
    The implementations are inefficient but should work.

--- tlibc/include/string.h
+++ tlibc/include/string.h
@@ -7,5 +7,6 @@ void* memcpy(void*, const void*, size_t);
 void* memmove(void*, const void*, size_t);
 void* memset(void*, int, size_t);
 size_t strlen(const char* str);
-
+size_t strnlen(const char *s, size_t maxlen);
+char *strncpy(char * dst, const char * src, size_t len);
 #endif
