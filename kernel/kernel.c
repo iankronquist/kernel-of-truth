177e1597f5e57390abcc92df2b2303e66f94790f
Ian Kronquist <iankronquist@gmail.com>
2016-07-17 20:40:43 -0400

    Partial boot module loading code in kernel main

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,5 +1,7 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
+
 #include <arch/x86/gdt.h>
 #include <arch/x86/idt.h>
 #include <arch/x86/io.h>
@@ -10,10 +12,10 @@
 #include <drivers/keyboard.h>
 #include <drivers/timer.h>
 
-
 #include <libk/kmem.h>
 #include <libk/kputs.h>
 #include <libk/klog.h>
+#include <libk/modloader.h>
 
 /* This little function exists to demonstrate the multi-processing
  * functionality. It spins and logs its progress. It takes no arguments and
@@ -23,6 +25,20 @@ void worker(void) {
     while(1) {
         klog("worker\n");
     }
+}
+
+// Heap allocated kernel command line.
+static char *command_line = NULL;
+// Size of the heap allocated kernel command line.
+static size_t command_line_size = 0;
+
+/* Copy the multiboot command line into heap allocated memory.
+ * Uses kmalloc, so it must be run after the heap is installed.
+ */
+static void copy_command_line(struct multiboot_info *mb) {
+    command_line_size = strnlen((char*)mb->cmdline, PAGE_SIZE);
+    command_line = kmalloc(command_line_size);
+    strncpy(command_line, (char*)mb->cmdline, PAGE_SIZE);
 }
 
 /* This is the entry point to the bulk of the kernel.
@@ -48,7 +64,14 @@ void kernel_main(struct multiboot_info *mb) {
     terminal_initialize();
     initialize_klog();
     kheap_install((struct kheap_metadata*)KHEAP_PHYS_ROOT, PAGE_SIZE);
+    copy_command_line(mb);
+    persist_multiboot_module_data(mb);
     physical_allocator_init(mb->mem_upper + mb->mem_lower);
+    (void)kernel_page_table_install(mb);
+    // Multiboot tables are not paged. Nullify the pointer so nobody tries to
+    // access it agaion.
+    mb = NULL;
+
     keyboard_install();
     char *hi = "Hello kernel!\n";
     void *testing = kmalloc(16);
@@ -56,7 +79,6 @@ void kernel_main(struct multiboot_info *mb) {
     kputs(testing);
     klog(testing);
     kfree(testing);
-    (void)kernel_page_table_install(mb);
 
     proc_init();
     struct process *worker_proc = create_proc(worker);
