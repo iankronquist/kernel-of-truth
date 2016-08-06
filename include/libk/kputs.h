1da5d178015b9e9d5dda912d0642d877d6b86f0e
Ian Kronquist <iankronquist@gmail.com>
2016-08-05 23:18:54 -0400

    Var args version of kprintf

--- include/libk/kputs.h
+++ include/libk/kputs.h
@@ -10,5 +10,6 @@
 void kputs(char *string);
 // Print a printf style formatted string to the VGA terminal.
 void kprintf(char *string, ...);
-
+// Variable arguments version of kprintf.
+void kvprintf(char* string, va_list args);
 #endif
