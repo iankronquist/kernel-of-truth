2976445bcd75498c010c0144585028851fca3b76
Ian Kronquist <iankronquist@gmail.com>
2016-08-09 18:36:04 -0400

    Add device.c to component.mk file

--- kernel/libk/component.mk
+++ kernel/libk/component.mk
@@ -1,1 +1,1 @@
-OBJ += build/kmem.libk.o build/kputs.libk.o build/physical_allocator.libk.o build/klog.libk.o build/kabort.libk.o build/vmem.libk.o
+OBJ += build/kmem.libk.o build/kputs.libk.o build/physical_allocator.libk.o build/klog.libk.o build/kabort.libk.o build/vmem.libk.o build/device.libk.o
