9246f203ef2ece6c51a04d090d77f109fedaf17c
Ian Kronquist <iankronquist@gmail.com>
2016-08-26 21:42:50 -0400

    Replace kputs with klog in kabort and kassert
    kputs can cause a panic because it has to write to mapped memory, while klog is unlikely too.

--- include/truth/kassert.h
+++ include/truth/kassert.h
@@ -1,11 +1,12 @@
 #pragma once
 
 #include <truth/kabort.h>
+#include <truth/klog.h>
 #include <truth/kputs.h>
 
 // An assertion macro. Print the location of the failure to the VGA terminal
 // and kernel panic.
 #define kassert(value) if (!(value)) { \
-    kprintf("Assertion failed: (%s) function %s, file %s, line %d.", #value, __FUNCTION__, __FILE__, __LINE__); \
+    klogf("Assertion failed: (%s) function %s, file %s, line %d.", #value, __FUNCTION__, __FILE__, __LINE__); \
     kabort();\
 }
