38a195764d189b30df98b2a782a3f798d6690229
Ian Kronquist <iankronquist@gmail.com>
2016-08-28 13:19:32 -0400

    Don't compile null device
    It conflicts with the terminal.

--- kernel/drivers/component.mk
+++ kernel/drivers/component.mk
@@ -1,1 +1,1 @@
-OBJ += build/terminal.driver.o build/serial_port.driver.o build/keyboard.driver.o build/timer.driver.o build/null.driver.o
+OBJ += build/terminal.driver.o build/serial_port.driver.o build/keyboard.driver.o build/timer.driver.o
