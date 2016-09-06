227282433f630e3c0f987fe3f0d4a9f03caf5fea
Ian Kronquist <iankronquist@gmail.com>
2016-09-06 19:26:10 -0400

    Refactor assembly kernel bootstrap code

--- kernel/arch/x86/boot.asm
+++ kernel/arch/x86/boot.asm
@@ -1,7 +1,20 @@
-MBALIGN    equ 1<<0
-MEMINFO  equ 1<<1
+extern bootstrap_paging
+extern kernel_main
+
+global _start
+global bootstrap_pdpt
+global bootstrap_page_dir
+global bootstrap_page_table
+global bootstrap_pdpt
+global bootstrap_stack_canary
+global bootstrap_stack_bottom
+global bootstrap_stack_top
+
+; Define symbols used in the multiboot header.
+MBALIGN  equ 1 << 0
+MEMINFO  equ 1 << 1
 FLAGS    equ MBALIGN | MEMINFO
-MAGIC    equ 0x1BADB002
+MAGIC    equ 0x1badb002
 CHECKSUM equ -(MAGIC + FLAGS)
 
 section .multiboot
@@ -11,26 +24,22 @@ align 4
 	dd CHECKSUM
 
 
-global _start
 _start:
 	; Enter protected mode
 	mov eax, cr0
 	or al, 1
 	mov cr0, eax
 
+	; Set up stack.
 	mov esp, bootstrap_stack_top
-	push ebx ; Multiboot header pointer
+	; Save multiboot header pointer.
+	push ebx
 
-	extern bootstrap_paging
+	; Set up bootstrap page tables.
 	call bootstrap_paging
 
 	mov eax, bootstrap_pdpt
 	mov cr3, eax
-
-	xor		edx,	edx
-	mov		eax,	(1 << 11)
-	mov		ecx,	0xc0000080
-	wrmsr
 
 	; Use PAE paging.
 	mov		eax,	cr4
@@ -40,40 +49,34 @@ _start:
 	; Enable paging and ring 0 write protection.
 	mov		eax,	cr0
 	or		eax,	(1 << 31) | (1 << 16)
-global paging_on
-paging_on:
 	mov		cr0,	eax
 
-	extern kernel_main
+	; Enter kernel_main, passing the multiboot header pointer.
 	call kernel_main
 
+	; If we ever unexpectedly return from kernel_main, clear interrupts and
+	; panic.
 	cli
 	hlt
-Lhang:
-	jmp Lhang
+hang:
+	jmp hang
 
 section .bss
 
 align 4096
-global bootstrap_page_dir
 bootstrap_page_dir:
 resb 4096
 
-global bootstrap_page_table
 bootstrap_page_table:
 resb 4096
 
-global bootstrap_stack_canary
 bootstrap_stack_canary:
 resb 4096
 
-global bootstrap_stack_bottom
 bootstrap_stack_bottom:
 resb 16384 ; 16 kb
-global bootstrap_stack_top
 bootstrap_stack_top:
 
 align 24
-global bootstrap_pdpt
 bootstrap_pdpt:
 resb 256
