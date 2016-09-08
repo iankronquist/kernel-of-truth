8b0fe12ade67385aa0aa09b0f20135a22a56df77
Ian Kronquist <iankronquist@gmail.com>
2016-09-07 20:29:40 -0400

    Refactor bootstrap paging to reuse headers

--- include/arch/x86/paging.h
+++ include/arch/x86/paging.h
@@ -1,53 +1,51 @@
 #pragma once
 
 #include <truth/types.h>
+#include <truth/memtypes.h>
 
 #include <contrib/multiboot.h>
 
-#define CUR_PAGE_DIRECTORY_ADDR ((uint32_t*)(0xfffff000))
+struct page_table;
 
-#define PAGE_PRESENT 1
-#define PAGE_WRITABLE 2
-#define PAGE_USER_MODE 4
-#define PAGE_WRITE_THROUGH 8
-#define PAGE_CACHE_DISABLE 16
-#define PAGE_ACCESSED 32
-#define PAGE_DIRTY 64
+#define PDPT_COUNT             4
+#define PAGE_DIR_ENTRY_COUNT   512
+#define PAGE_TABLE_ENTRY_COUNT 512
 
-// Set the current page table to the provided top level directory and enable
-// paging.
-extern void enable_paging(page_frame_t page_dir);
-// Disable paging.
-extern void disable_paging(void);
-// Do not change the page table, just flip the paging bit.
-extern void just_enable_paging(void);
-// Get the current page table directory.
-extern page_frame_t get_page_dir(void);
-// Flush the translation look-aside buffer.
-extern void flush_tlb(void);
+#define PT_SHIFT   12
+#define PD_SHIFT   21
+#define PDPT_SHIFT 30
 
-// Install the bootstrap kernel page table.
-page_frame_t bootstrap_kernel_page_table(void);
+#define PT_MASK    0x001ff000
+#define PD_MASK    0x3fe00000
+#define PDPT_MASK  0xc0000000
 
-// Create a new page directory and map important data.
-// Map code at entrypoint, the code location, and the code's stack.
-page_frame_t create_page_dir(void *link_loc, void *stack_loc,
-        void(*entrypoint)(), uint16_t permissions);
+enum memory_permissions {
+    perm_none           = 0,
+    perm_present        = (1ull << 0),
+    perm_write          = (1ull << 1),
+    perm_user_mode      = (1ull << 2),
+    perm_write_through  = (1ull << 3),
+    perm_uncached       = (1ull << 4),
+    perm_not_executable = (1ull << 63),
+};
 
-// Map a page when paging is disabled.
-// Map a physical page in the page directory at the given virtual address with
-// permissions.
-int map_page(uint32_t *page_dir, page_frame_t physical_page,
-        void *virtual_address, uint16_t permissions);
+static inline size_t pdpt_index(void *addr) {
+    return (size_t)((uintptr_t)addr >> PDPT_SHIFT);
+}
 
-// Unmap a page from a different page directory than the current one.
-// if should_free_frame is true, the resident page frame will be marked as
-// free.
-int inner_unmap_page(uint32_t *page_entries, void *virtual_address,
-        bool should_free_frame);
+static inline size_t page_dir_index(void *addr) {
+    return (size_t)(((uintptr_t)addr & PD_MASK) >> PD_SHIFT);
+}
 
-// Free a page table.
-void free_table(uint32_t *page_dir);
-// Map a page into a different page table than the current one.
-int inner_map_page(uint32_t *page_dir, page_frame_t physical_page,
-        void *virtual_address, uint16_t permissions);
+static inline size_t page_table_index(void *addr) {
+    return (size_t)(((uintptr_t)addr & PT_MASK) >> PT_SHIFT);
+}
+
+
+status_t checked map_page(struct page_table*, void*, phys_addr_t,
+        enum memory_permissions);
+status_t checked unmap_page(struct page_table*, void*, phys_addr_t);
+
+status_t checked create_page_table(struct page_table**);
+
+struct page_table *get_cur_page_table(void);
--- kernel/arch/x86/bootstrap_paging.c
+++ kernel/arch/x86/bootstrap_paging.c
@@ -3,38 +3,7 @@
 #include <truth/memtypes.h>
 #include <truth/private/memlayout.h>
 
-#define PDPT_COUNT             4
-#define PAGE_DIR_ENTRY_COUNT   512
-#define PAGE_TABLE_ENTRY_COUNT 512
-
-#define PT_SHIFT   12
-#define PD_SHIFT   21
-#define PDPT_SHIFT 30
-
-#define PT_MASK  0x001ff000
-#define PD_MASK  0x3fe00000
-
-enum memory_permissions {
-    perm_none           = 0,
-    perm_present        = (1ull << 0),
-    perm_write          = (1ull << 1),
-    perm_user_mode      = (1ull << 2),
-    perm_write_through  = (1ull << 3),
-    perm_uncached       = (1ull << 4),
-    perm_not_executable = (1ull << 63),
-};
-
-static inline size_t pdpt_index(void *addr) {
-    return (size_t)((uintptr_t)addr >> PDPT_SHIFT);
-}
-
-static inline size_t page_dir_index(void *addr) {
-    return (size_t)(((uintptr_t)addr & PD_MASK) >> PD_SHIFT);
-}
-
-static inline size_t page_table_index(void *addr) {
-    return (size_t)(((uintptr_t)addr & PT_MASK) >> PT_SHIFT);
-}
+#include <arch/x86/paging.h>
 
 extern uint64_t bootstrap_pdpt[PDPT_COUNT];
 extern uint64_t bootstrap_page_dir[PAGE_DIR_ENTRY_COUNT];
@@ -65,7 +34,7 @@ void bootstrap_paging(void) {
 
     // Fractal paging.
     bootstrap_page_dir[PAGE_DIR_ENTRY_COUNT-1] =
-        (uintptr_t)bootstrap_page_dir | perm_present;
+        (uintptr_t)bootstrap_page_dir | perm_present | perm_write;
 
     // Unmap stack canary.
     bootstrap_page_table[page_table_index((void*)KERNEL_START +
