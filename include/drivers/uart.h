699eb8ba459ac4e4f67b7fe5d745a63f157a4219
Ian Kronquist <iankronquist@gmail.com>
2016-09-09 18:56:23 -0400

    And let's try that all again from the top!
    Kernel of Truth v2. Goodbye code, you've taught be a lot of lessons, and we may
    pull a few files back in. But for now it's time to start fresh and do it
    *right*.

--- include/drivers/uart.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <truth/device.h>
-
-extern const struct device uart_char_device;
-
-// Register and initialize the uart device.
-void register_uart_device(void);
--- kernel/drivers/component.mk
+++
@@ -1,4 +0,0 @@
-OBJ += build/terminal.driver.o
-OBJ += build/serial_port.driver.o
-OBJ += build/keyboard.driver.o
-OBJ += build/timer.driver.o
