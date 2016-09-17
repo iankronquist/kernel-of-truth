c3a4e30a469da386940c94901e4f59b57b78284e
Ian Kronquist <iankronquist@gmail.com>
2016-09-17 18:44:50 -0400

    Remove unused macro and fix panic macro

--- include/truth/panic.h
+++ include/truth/panic.h
@@ -3,20 +3,9 @@
 #include <truth/cpu.h>
 #include <truth/log.h>
 
-#define not_implemented(x) { \
-        logf(Log_Error, "%s is not yet implemented\n", x); \
-        logf(Log_Error, "Pull requests are welcome! " \
-                "Visit %s to learn more.\n", \
-                project_website); \
-        logf(Log_Error, "Function %s, file %s, line %d.", __func__, __FILE__, \
-                __LINE__); \
-        panic(); \
-    }
-
 #define assert(x) \
     if (!(x)) { \
-        logf(Log_Error, \
-                "Assertion failed: (%s) function %s, file %s, line %d.", \
+        logf("Assertion failed: (%s) function %s, file %s, line %d.", \
                 (#x), __func__, __FILE__, __LINE__); \
         panic(); \
     }
