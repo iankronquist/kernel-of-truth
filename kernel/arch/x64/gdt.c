bf6b09b70f530722ed97d1ff9a641b3ae1fa62ba
Ian Kronquist <iankronquist@gmail.com>
2016-09-18 13:13:22 -0400

    Rewrite GDT macros

--- kernel/arch/x64/gdt.c
+++ kernel/arch/x64/gdt.c
@@ -50,7 +50,7 @@ struct gdt_entry64 {
 
 struct tss_entry {
     uint32_t reserved0;
-    uint64_t stack0;     /* This is not naturally aligned, so packed is needed. */
+    uint64_t stack0;
     uint64_t stack1;
     uint64_t stack2;
     uint64_t reserved1;
@@ -77,46 +77,49 @@ struct tss_entry Tss = {
 #define GRAN_32_BIT_MODE (1 << 6)
 #define GRAN_4KIB_BLOCKS (1 << 7)
 
-#define GDT_ENTRY(base, limit, access, granularity) \
-    { (limit) & 0xffff,                              /* limit_low */ \
-      (uint16_t) ((base) >> 0 & 0xffff),             /* base_low */ \
-      (uint8_t) ((base) >> 16 & 0xff),               /* base_middle */ \
-      (access) & 0xff,                               /* access */ \
-      ((limit) >> 16 & 0x0f) | ((granularity) & 0xf0),     /* granularity */ \
-      (uint8_t) ((base) >> 24 & 0xff),                 /* base_high */ }
+#define gdt_entry(base, limit, access, granularity) \
+    { (limit) & 0xffff, \
+      (uint16_t) ((base) >> 0  & 0xffff), \
+      (uint8_t)  ((base) >> 16 & 0xff), \
+      (access) & 0xff, \
+      ((limit) >> 16 & 0x0f) | ((granularity) & 0xf0), \
+      (uint8_t) ((base) >> 24 & 0xff), \
+    }
 
-#define GDT_ENTRY64(base, limit, access, granularity) \
-    { (limit) & 0xffff,                              /* limit_low */ \
-      (uint16_t) ((base) >> 0 & 0xffff),             /* base_low */ \
-      (uint8_t) ((base) >> 16 & 0xff),               /* base_middle */ \
-      (access) & 0xff,                               /* access */ \
-      ((limit) >> 16 & 0x0f) | ((granularity) & 0xf0),     /* granularity */ \
-      (uint8_t) ((base) >> 24 & 0xff),                 /* base_high */ }, \
-    { (uint16_t) ((base) >> 32 & 0xffff),            /* base_highest */ \
-      (uint16_t) ((base) >> 48 & 0xffff),            /* base_highest */ \
-      0,                                             /* reserved0 */ \
-      0,                                             /* reserved0 */ \
-      0,                                             /* reserved0 */ \
-      0,                                               /* reserved0 */ }
+#define gdt_entry64(base, limit, access, granularity) \
+    { (limit) & 0xffff, \
+      (uint16_t) ((base) >> 0  & 0xffff), \
+      (uint8_t)  ((base) >> 16 & 0xff), \
+      (access) & 0xff, \
+      ((limit) >> 16 & 0x0f) | ((granularity) & 0xf0), \
+      (uint8_t) ((base) >> 24 & 0xff), \
+    }, \
+    { (uint16_t) ((base) >> 32 & 0xffff), \
+      (uint16_t) ((base) >> 48 & 0xffff), \
+      0, \
+      0, \
+      0, \
+      0, \
+    } \
 
 struct gdt_entry Gdt[] = {
     // NULL segment
-    GDT_ENTRY(0, 0, 0, 0),
+    gdt_entry(0, 0, 0, 0),
 
     // Kernel Code Segment.
-    GDT_ENTRY(0, 0xffffffff, 0x9a, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
+    gdt_entry(0, 0xffffffff, 0x9a, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
 
     // Kernel Data Segment.
-    GDT_ENTRY(0, 0xffffffff, 0x92, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
+    gdt_entry(0, 0xffffffff, 0x92, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
 
     // User Code Segment.
-    GDT_ENTRY(0, 0xffffffff, 0xfa, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
+    gdt_entry(0, 0xffffffff, 0xfa, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
 
     // User Data Segment.
-    GDT_ENTRY(0, 0xffffffff, 0xf2, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
+    gdt_entry(0, 0xffffffff, 0xf2, GRAN_64_BIT_MODE | GRAN_4KIB_BLOCKS),
 
     // Task Switch Segment.
-    GDT_ENTRY64(0ull, sizeof(Tss) - 1, 0xe9, 0x00),
+    gdt_entry64(0ull, sizeof(Tss) - 1, 0xe9, 0x00),
 };
 
 uint16_t Gdt_Size = sizeof(Gdt) - 1;
