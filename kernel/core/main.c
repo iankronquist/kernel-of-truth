55662d5c57bbd169747d58cd26319062054a6303
Ian Kronquist <iankronquist@gmail.com>
2016-09-18 01:05:13 -0400

    Add project website to startup message

--- kernel/core/main.c
+++ kernel/core/main.c
@@ -7,6 +7,8 @@ void kernel_main(void *multiboot_tables) {
     init_interrupts();
     enum status unused(status) = init_log("log");
     init_physical_allocator(multiboot_tables);
-    logf("The Kernel of Truth\n\tVersion %d.%d.%d\n\tCommit %s\n", kernel_major, kernel_minor, kernel_patch, vcs_version);
+    logf("The Kernel of Truth\n\tVersion %d.%d.%d\n\tCommit %s\n\t%s\n",
+         kernel_major, kernel_minor, kernel_patch, vcs_version,
+         project_website);
     halt();
 }
