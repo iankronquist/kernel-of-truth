0cf80a27725f9c13e717e00f75be7536db3b617b
Ian Kronquist <iankronquist@gmail.com>
2016-10-06 22:23:46 -0400

    Fix a variety of bugs in paging functions

--- kernel/arch/x64/paging.c
+++ kernel/arch/x64/paging.c
@@ -1,11 +1,15 @@
 #include <arch/x64/paging.h>
+#include <truth/log.h>
 #include <truth/physical_allocator.h>
+#include <truth/panic.h>
 #include <truth/types.h>
 
-#define pl4_count 512
-#define pl3_count 512
-#define pl2_count 512
+extern void invalidate_tlb(void);
+
 #define pl1_count 512
+#define pl2_count 512
+#define pl3_count 512
+#define pl4_count 512
 
 #define pl1_offset 12
 #define pl1_mask   0x1ff
@@ -39,7 +43,7 @@ extern void write_cr3(phys_addr value);
 extern uint64_t read_cr3(void);
 
 static inline size_t pl4_index(void *address) {
-    return (uintptr_t)address >> pl4_offset;
+    return (uintptr_t)address >> pl4_offset & pl4_mask;
 }
 
 static inline size_t pl3_index(void *address) {
@@ -62,18 +66,25 @@ static struct page_table *current_page_table(void) {
     return (struct page_table *)0xfffffffffffff000;
 }
 
+#define fractal_region 0xffffff8000000000
+
+#define page_size 4 * KB
+
 static pl3 *get_pl3(void *address) {
-    return (pl3 *)(0xffffffffffe00000 + 0x1000 * pl3_index(address));
+    return (pl2 *)(0xffffffffffe00000 | page_size * pl4_index(address));
 }
 
 static pl2 *get_pl2(void *address) {
-    return (pl2 *)(0xffffffffc0000000 + 0x200000 * pl3_index(address) + 0x1000 *
-                   pl2_index(address));
+    return (pl2 *)(0xffffffffc0000000 |
+                   pl3_count * page_size * pl4_index(address) |
+                   page_size * pl3_index(address));
 }
 
 static pl1 *get_pl1(void *address) {
-    return (pl1 *)(0xffffff8000000000 + 0x40000000 * pl3_index(address) +
-                   0x200000 * pl2_index(address));
+    return (pl1 *)(0xffffff8000000000 |
+                   pl3_count * pl2_count * page_size * pl4_index(address) |
+                   pl2_count * page_size * pl3_index(address) |
+                   page_size * pl2_index(address));
 }
 
 static inline bool is_pl3_present(struct page_table *page_table,
@@ -108,11 +119,13 @@ enum status checked map_page(void *virtual_address, phys_addr phys_address,
                              enum page_attributes permissions) {
 
     struct page_table *page_table = current_page_table();
+    logf("Mapping: %p -> %p\n", virtual_address, phys_address);
     if (!is_pl3_present(page_table, virtual_address)) {
         phys_addr phys_address = physical_alloc(1);
         page_table->entries[pl4_index(virtual_address)] =
             (phys_address | permissions | page_user_access |
              page_present);
+        invalidate_tlb();
     }
 
     pl3 *level_three = get_pl3(virtual_address);
@@ -121,6 +134,7 @@ enum status checked map_page(void *virtual_address, phys_addr phys_address,
         (*level_three)[pl3_index(virtual_address)] =
             (phys_address | permissions | page_user_access |
              page_present);
+        invalidate_tlb();
     }
 
     pl2 *level_two = get_pl2(virtual_address);
@@ -129,15 +143,21 @@ enum status checked map_page(void *virtual_address, phys_addr phys_address,
         (*level_two)[pl2_index(virtual_address)] =
             (phys_address | permissions | page_user_access |
              page_present);
+        invalidate_tlb();
     }
 
     pl1 *level_one = get_pl1(virtual_address);
     if (is_page_present(level_one, virtual_address)) {
+        logf("The virtual address %p is already present\n", virtual_address);
         return Error_Present;
     }
 
-    (*level_one)[pl1_index(virtual_address)] = (phys_address |
-                                                permissions);
+    (*level_one)[pl1_index(virtual_address)] = (phys_address | permissions | page_present);
+    invalidate_tlb();
+
+    int *test = virtual_address;
+    logf("Testing: %p\n", test[0]);
+
     return Ok;
 }
 
