06c472330a5c95c71e4f0f87e2da52587534c8f2
Ian Kronquist <iankronquist@gmail.com>
2016-10-16 18:46:39 -0400

    Slight formatting in hashtable comments

--- include/truth/hashtable.h
+++ include/truth/hashtable.h
@@ -5,6 +5,7 @@
 // Function used for hashing an object. Should return a good hash in the range
 // [0, SIZE_T_MAX].
 typedef size_t (*hash_f)(void *);
+
 // Compare two objects when fetching from a hash table.
 typedef enum partial (*partial_comp_f)(void *, void *);
 
@@ -29,6 +30,7 @@ enum partial hash_str_comp(void *key_a, void *key_b);
 
 // Create and allocate a new hash table.
 struct hashtable *hashtable_init(size_t size, hash_f hf, partial_comp_f hc);
+
 // Free a hash table.
 void hashtable_destroy(struct hashtable *ht);
 
@@ -37,9 +39,11 @@ void hashtable_destroy(struct hashtable *ht);
 // Does not free the item.
 // May resize the hash table.
 enum status checked hashtable_remove(struct hashtable *ht, void *key);
+
 // Get an item associated with a key from the hash table.
 // Return NULL if the item is absent.
 void *hashtable_get(struct hashtable *ht, void *key);
+
 // Put an item in the hash table.
 // Does not copy the item.
 // May resize the hash table.
