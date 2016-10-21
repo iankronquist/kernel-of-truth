5a3d7873993eb4c6f4e4d650e2b46b0d403fecd3
Ian Kronquist <iankronquist@gmail.com>
2016-10-21 19:32:17 -0400

    Print CPU time in splash screen

--- kernel/core/main.c
+++ kernel/core/main.c
@@ -21,9 +21,9 @@ void kernel_main(void *multiboot_tables) {
     enum status status = init_log("log");
     assert(status == Ok);
     log(Logo);
-    logf("\tVersion %d.%d.%d\n\tCommit %s\n\t%s\n",
+    logf("\tVersion %d.%d.%d\n\tCommit %s\n\t%s\n\tCPU Time %ld\n",
          kernel_major, kernel_minor, kernel_patch, vcs_version,
-         project_website);
+         project_website, cpu_time());
     init_interrupts();
     init_physical_allocator(multiboot_tables);
     init_slab();
