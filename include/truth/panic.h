9143b208eeb766ad49c554cb53a23cedf73333ff
Ian Kronquist <iankronquist@gmail.com>
2016-10-31 17:20:56 -0400

    Add newlines to the end of asserts

--- include/truth/panic.h
+++ include/truth/panic.h
@@ -8,7 +8,7 @@
 
 #define assert(x) \
     if (!(x)) { \
-        logf("Assertion failed: (%s) function %s, file %s, line %d.", \
+        logf("Assertion failed: (%s) function %s, file %s, line %d.\n", \
              (#x), __func__, __FILE__, __LINE__); \
         panic(); \
     }
@@ -16,7 +16,7 @@
 #define assert_ok(x) \
     if (x != Ok) { \
         logf("Assertion failed (%s) status: %s, function %s, file %s, " \
-             "line %d.", (#x), status_message(x), __func__, __FILE__, \
+             "line %d.\n", (#x), status_message(x), __func__, __FILE__, \
              __LINE__); \
         panic(); \
     }
