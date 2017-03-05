859a2b7337907df6bdd41c069cca285d17521b51
Ian Kronquist <iankronquist@gmail.com>
2017-03-05 14:30:47 -0500

    Refactor boot.S & headers

--- include/truth/memory_sizes.h
+++ include/truth/memory_sizes.h
@@ -8,15 +8,20 @@
 #define Page_Small  (4 * KB)
 #define Page_Medium (2 * MB)
 #define Page_Large  (GB)
+
+#define Memory_Present       (1 << 0)
+#define Memory_Writable      (1 << 1)
+#define Memory_User_Access   (1 << 2)
+#define Memory_No_Execute    (1 << 63)
+
+
 #elif __C__
 enum page_size {
     Page_Small  = (4 * KB),
     Page_Medium = (2 * MB),
     Page_Large  = (GB),
 };
-#endif
 
-#ifdef __C__
 enum memory_attributes {
     Memory_No_Attributes = (0),
     Memory_Present       = (1ul << 0),
