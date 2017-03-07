b2677ff46f6427f3ee03f545f964285674f5e8c4
Ian Kronquist <iankronquist@gmail.com>
2017-03-07 18:57:25 -0500

    Move ISRs into separate file

--- kernel/arch/x64/idt.c
+++ kernel/arch/x64/idt.c
@@ -3,9 +3,10 @@
 #include <truth/panic.h>
 #include <truth/types.h>
 
+#include <arch/x64/control_registers.h>
+#include <arch/x64/isr.h>
 #include <arch/x64/pic.h>
 #include <arch/x64/port.h>
-#include <arch/x64/control_registers.h>
 
 #define IDT_Size 256
 
@@ -40,46 +41,8 @@ static isr_f *Interrupt_Dispatch[IDT_Size] = {0};
 
 extern void idt_load(struct idt_ptr *);
 
-extern void _service_interrupt(void);
 
-extern void isr0(void);
-extern void isr1(void);
-extern void isr2(void);
-extern void isr3(void);
-extern void isr4(void);
-extern void isr5(void);
-extern void isr6(void);
-extern void isr7(void);
-extern void isr8(void);
-extern void isr9(void);
-extern void isr10(void);
-extern void isr11(void);
-extern void isr12(void);
-extern void isr13(void);
-extern void isr14(void);
-extern void isr15(void);
-extern void isr16(void);
-extern void isr17(void);
-extern void isr18(void);
-extern void isr19(void);
-extern void isr20(void);
-extern void isr21(void);
-extern void isr22(void);
-extern void isr23(void);
-extern void isr24(void);
-extern void isr25(void);
-extern void isr26(void);
-extern void isr27(void);
-extern void isr28(void);
-extern void isr29(void);
-extern void isr30(void);
-extern void isr31(void);
-extern void isr32(void);
-extern void isr33(void);
-extern void isr34(void);
-
-/* Set an entry in the idt.
- */
+/* Set an entry in the idt.  */
 static void idt_set_gate(uint8_t num, uintptr_t base,
                          uint16_t segment_selector, uint8_t flags) {
     IDT[num].base_low = base & 0xffff;
