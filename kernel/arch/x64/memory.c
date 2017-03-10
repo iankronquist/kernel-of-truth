9c10b911aa80dac7d4dd3907ab1d0e14982160a7
Ian Kronquist <iankronquist@gmail.com>
2017-03-09 19:00:42 -0500

    Don't use magic CPUID values

--- kernel/arch/x64/memory.c
+++ kernel/arch/x64/memory.c
@@ -1,21 +1,21 @@
 #include <arch/x64/cpu.h>
 #include <truth/log.h>
 
+
 void supervisor_memory_protection_init(void) {
     uint32_t eax, ebx, ecx, edx;
     eax = 7;
     ecx = 0;
     cpuid(&eax, &ebx, &ecx, &edx);
     if (ebx & CPUID_SMEP) {
-        log(Log_Error, "smep");
-        cpu_cr4_set_bit(20);
+        log(Log_Info, "SMEP enabled.");
+        cpu_cr4_set_bit(CPU_CR4_SMEP_BIT);
     }
 
     if (ebx & CPUID_SMAP) {
-        log(Log_Error, "smap");
-        cpu_cr4_set_bit(21);
+        log(Log_Info, "SMAP enabled.");
+        cpu_cr4_set_bit(CPU_CR4_SMAP_BIT);
     }
-    log(Log_Error, "smp");
 }
 
 void memory_init(void) {
