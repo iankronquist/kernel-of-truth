3352e265e81dc759b8d5e3cf7ecccb4db17980ee
Ian Kronquist <iankronquist@gmail.com>
2017-03-10 22:44:57 -0500

    Rewrite scheduler API

--- include/truth/scheduler.h
+++ include/truth/scheduler.h
@@ -1,8 +1,10 @@
 #pragma once
 
-enum status scheduler_init(void);
-enum status scheduler_fini(void);
+#include <truth/process.h>
+
+enum status scheduler_init(struct process *);
+void scheduler_fini(void);
 void scheduler_yield(void);
-struct process *scheduler_current_process(void);
-enum status scheduler_add_process(struct process *proc);
-enum status scheduler_remove_process(struct process *proc);
+struct thread *scheduler_current_thread(void);
+void scheduler_add_thread(struct thread *thread);
+void scheduler_remove_thread(struct thread *thread);
