828ef32fae186f9a2afb32969db9923df1ee3890
Ian Kronquist <iankronquist@gmail.com>
2017-03-11 00:16:37 -0500

    Properly cast container_of result

--- include/truth/types.h
+++ include/truth/types.h
@@ -72,7 +72,7 @@ extern uint8_t __kernel_end;
 #define phys_to_virt(a) ((void *)a)
 
 #define container_of(child, parent_type, parent_entry) \
-    (child - &((parent_type)NULL)->parent_entry)
+    ((parent_type)(child - &((parent_type)NULL)->parent_entry))
 #define is_aligned(value, alignment) !(value & (alignment - 1))
 #define round_next(x, y) (((x) + (y - 1)) & ~(y - 1))
 #define static_array_count(x) (sizeof(x) / sizeof(x)[0])
