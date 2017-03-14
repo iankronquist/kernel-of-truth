3c7f4b1e96dc452fa4ff5cd7cd6223bf7b2198c1
Ian Kronquist <iankronquist@gmail.com>
2017-03-13 22:42:48 -0400

    Nuke lower half slab allocator region vector

--- kernel/core/slab.c
+++ kernel/core/slab.c
@@ -7,21 +7,15 @@
 #include <truth/region_vector.h>
 #include <truth/memory.h>
 
-static struct lock slab_lower_half_lock = Lock_Clear;
 static struct lock slab_higher_half_lock = Lock_Clear;
 
-extern struct region_vector slab_lower_half;
 extern struct region_vector slab_higher_half;
 
 
-static inline bool in_lower_half(void *address) {
-    return address <= Lower_Half_End;
-}
 
-
-static void *slab_alloc_helper(size_t bytes, phys_addr *phys,
-                               enum memory_attributes page_attributes,
-                               struct region_vector *vect) {
+void *slab_alloc_helper(size_t bytes, phys_addr *phys,
+                        enum memory_attributes page_attributes,
+                        struct region_vector *vect) {
     if (!is_aligned(bytes, Page_Small)) {
         logf(Log_Error, "unaligned %lx %x\n", bytes, Page_Small);
         return NULL;
@@ -72,46 +66,30 @@ static void slab_free_helper(size_t bytes, void *address,
 void slab_init(void) {
     union address address;
     region_vector_init(&slab_higher_half);
-    region_vector_init(&slab_lower_half);
-    address.virtual = (void *)(4 * MB);
-    region_free(&slab_lower_half, address, Lower_Half_Size - (4 * MB));
     address.virtual = Kernel_Virtual_End;
     region_free(&slab_higher_half, address, Higher_Half_Size - Kernel_Image_Size - (4 * MB));
 }
 
 
 void slab_free(size_t bytes, void *address) {
-    if (in_lower_half(address)) {
-        lock_acquire_writer(&slab_lower_half_lock);
-        slab_free_helper(bytes, address, &slab_lower_half, true);
-        lock_release_writer(&slab_lower_half_lock);
-    } else {
-        lock_acquire_writer(&slab_higher_half_lock);
-        slab_free_helper(bytes, address, &slab_higher_half, true);
-        lock_release_writer(&slab_higher_half_lock);
-    }
+    assert(!memory_is_lower_half(address));
+    lock_acquire_writer(&slab_higher_half_lock);
+    slab_free_helper(bytes, address, &slab_higher_half, true);
+    lock_release_writer(&slab_higher_half_lock);
 }
 
 
 void slab_free_virt(size_t bytes, void *address) {
-    if (in_lower_half(address)) {
-        lock_acquire_writer(&slab_lower_half_lock);
-        slab_free_helper(bytes, address, &slab_lower_half, false);
-        lock_release_writer(&slab_lower_half_lock);
-    } else {
-        lock_acquire_writer(&slab_higher_half_lock);
-        slab_free_helper(bytes, address, &slab_higher_half, false);
-        lock_release_writer(&slab_higher_half_lock);
-    }
+    assert(!memory_is_lower_half(address));
+    lock_acquire_writer(&slab_higher_half_lock);
+    slab_free_helper(bytes, address, &slab_higher_half, false);
+    lock_release_writer(&slab_higher_half_lock);
 }
 
 
 void slab_free_locked(size_t bytes, void *address) {
-    if (in_lower_half(address)) {
-        slab_free_helper(bytes, address, &slab_lower_half, true);
-    } else {
-        slab_free_helper(bytes, address, &slab_higher_half, true);
-    }
+    assert(!memory_is_lower_half(address));
+    slab_free_helper(bytes, address, &slab_higher_half, true);
 }
 
 
@@ -119,13 +97,10 @@ void *slab_alloc_locked(size_t bytes, enum memory_attributes page_attributes) {
     phys_addr phys;
     void *virt;
 
-    if (page_attributes & Memory_User_Access) {
-        virt = slab_alloc_helper(bytes, &phys, page_attributes,
-                                 &slab_lower_half);
-    } else {
-        virt = slab_alloc_helper(bytes, &phys, page_attributes,
-                                 &slab_higher_half);
-    }
+    assert(!(page_attributes & Memory_User_Access));
+
+    virt = slab_alloc_helper(bytes, &phys, page_attributes,
+                             &slab_higher_half);
 
     return virt;
 }
@@ -136,17 +111,11 @@ void *slab_alloc(size_t bytes, enum memory_attributes page_attributes) {
     phys_addr phys;
     void *virt;
 
-    if (page_attributes & Memory_User_Access) {
-        lock_acquire_writer(&slab_lower_half_lock);
-        virt = slab_alloc_helper(bytes, &phys, page_attributes,
-                                 &slab_lower_half);
-        lock_release_writer(&slab_lower_half_lock);
-    } else {
-        lock_acquire_writer(&slab_higher_half_lock);
-        virt = slab_alloc_helper(bytes, &phys, page_attributes,
-                                 &slab_higher_half);
-        lock_release_writer(&slab_higher_half_lock);
-    }
+    assert(!(page_attributes & Memory_User_Access));
+
+    lock_acquire_writer(&slab_higher_half_lock);
+    virt = slab_alloc_helper(bytes, &phys, page_attributes, &slab_higher_half);
+    lock_release_writer(&slab_higher_half_lock);
 
     return virt;
 }
@@ -154,16 +123,12 @@ void *slab_alloc(size_t bytes, enum memory_attributes page_attributes) {
 
 void *slab_alloc_phys(phys_addr *phys, enum memory_attributes page_attributes) {
     void *virt;
-    if (page_attributes & Memory_User_Access) {
-        lock_acquire_writer(&slab_lower_half_lock);
-        virt = slab_alloc_helper(Page_Small, phys, page_attributes,
-                                 &slab_lower_half);
-        lock_release_writer(&slab_lower_half_lock);
-    } else {
-        lock_acquire_writer(&slab_higher_half_lock);
-        virt = slab_alloc_helper(Page_Small, phys, page_attributes,
-                                 &slab_higher_half);
-        lock_release_writer(&slab_higher_half_lock);
-    }
+
+    assert(!(page_attributes & Memory_User_Access));
+
+    lock_acquire_writer(&slab_higher_half_lock);
+    virt = slab_alloc_helper(Page_Small, phys, page_attributes,
+                             &slab_higher_half);
+    lock_release_writer(&slab_higher_half_lock);
     return virt;
 }
