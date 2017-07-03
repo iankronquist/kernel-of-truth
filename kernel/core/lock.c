f1c52ab2f6f2aace272ab994bf7668a807b4a70c
Ian Kronquist <iankronquist@gmail.com>
2017-07-02 20:45:44 -0400

    Upgrade locking memory ordering types

--- kernel/core/lock.c
+++ kernel/core/lock.c
@@ -8,26 +8,32 @@ void lock_clear(struct lock *lock) {
 
 void lock_acquire_reader(struct lock *lock) {
     atomic_bool expected = false;
-    while (!atomic_compare_exchange_weak(&lock->writer, &expected, true));
-    atomic_fetch_add_explicit(&lock->readers, 1, memory_order_acquire);
-    atomic_store_explicit(&lock->writer, false, memory_order_release);
+    while (!atomic_compare_exchange_weak_explicit(&lock->writer, &expected, true, memory_order_acq_rel, memory_order_acq_rel));
+    atomic_fetch_add_explicit(&lock->readers, 1, memory_order_acq_rel);
+    atomic_store_explicit(&lock->writer, false, memory_order_acq_rel);
 }
 
 void lock_release_reader(struct lock *lock) {
-    atomic_fetch_sub_explicit(&lock->readers, 1, memory_order_release);
+    atomic_fetch_sub_explicit(&lock->readers, 1, memory_order_acq_rel);
 }
 
 void lock_acquire_writer(struct lock *lock) {
     atomic_bool expected_writer = false;
-    while (!atomic_compare_exchange_weak(&lock->writer, &expected_writer,
-            true)) {
-        while (atomic_load_explicit(&lock->readers, memory_order_acquire) ==
-                0);
+    while (!atomic_compare_exchange_weak_explicit(&lock->writer, &expected_writer, true, memory_order_acq_rel, memory_order_acq_rel)) {
+        while (atomic_load_explicit(&lock->readers, memory_order_acq_rel) == 0);
     }
 }
 
 void lock_release_writer(struct lock *lock) {
-    assert(atomic_load_explicit(&lock->writer, memory_order_acquire) == true);
-    assert(atomic_load_explicit(&lock->readers, memory_order_acquire) == 0);
-    atomic_store_explicit(&lock->writer, false, memory_order_release);
+    assert(atomic_load_explicit(&lock->writer, memory_order_acq_rel) == true);
+    assert(atomic_load_explicit(&lock->readers, memory_order_acq_rel) == 0);
+    atomic_store_explicit(&lock->writer, false, memory_order_acq_rel);
+}
+
+bool lock_is_writer_acquired(struct lock *lock) {
+    return atomic_load_explicit(&lock->writer, memory_order_acq_rel);
+}
+
+bool lock_is_reader_acquired(struct lock *lock) {
+    return atomic_load_explicit(&lock->readers, memory_order_acq_rel) > 0;
 }
