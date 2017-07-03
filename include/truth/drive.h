a02c15d6fab3b45497eb1f38be6c38b50a194731
Ian Kronquist <iankronquist@gmail.com>
2017-07-02 20:46:54 -0400

    Define drive_buffer

---
+++ include/truth/drive.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <truth/file.h>
+
+enum drive_status {
+    Drive_Buffer_Error = 0x1,
+    Drive_Buffer_Read  = 0x2,
+    Drive_Buffer_Dirty = 0x4,
+};
+
+#define Drive_Sector_Size 512
+#define Drive_Buffer_Size 512
+
+struct drive_buffer {
+  enum drive_status flags;
+  unsigned int dev;
+  unsigned int block_number;
+  struct lock lock;
+  struct drive_buffer *next;
+  unsigned char data[Drive_Buffer_Size];
+};
