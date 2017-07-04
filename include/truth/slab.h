6516fff6d01362aa556367a18013b8d8cda91cec
Ian Kronquist <iankronquist@gmail.com>
2017-07-04 18:25:24 -0400

    Rename misleading slab arguments
    Already named properly in implementation.

--- include/truth/slab.h
+++ include/truth/slab.h
@@ -8,8 +8,8 @@ void slab_init(void);
 
 void *slab_alloc(size_t bytes, enum memory_attributes attrs);
 void *slab_alloc_phys(phys_addr *phys, enum memory_attributes attrs);
-void slab_free(size_t count, void *address);
-void slab_free_virt(size_t count, void *address);
+void slab_free(size_t bytes, void *address);
+void slab_free_virt(size_t bytes, void *address);
 
 void *slab_alloc_locked(size_t bytes, enum memory_attributes attrs);
 void slab_free_locked(size_t count, void *address);
