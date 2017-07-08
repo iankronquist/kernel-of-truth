e72c89ffc34a9ad44688a96ed25101bba4335c97
Ian Kronquist <iankronquist@gmail.com>
2017-07-07 22:31:44 -0400

    Fix wrong inline asm arguments for cpuid

--- include/arch/x64/cpu.h
+++ include/arch/x64/cpu.h
@@ -16,7 +16,7 @@ static inline void cpuid(uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *
             "=b"(*ebx),
             "=c"(*ecx),
             "=d"(*edx)
-            : "0" (*eax), "2" (*ecx)
+            : "0" (*eax)
             :);
 }
 
