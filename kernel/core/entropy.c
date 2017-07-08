f7703b9e10a572de7d5ff15434e3845a0a43043a
Ian Kronquist <iankronquist@gmail.com>
2017-07-07 21:29:20 -0400

    Give up and just use reader-writer locks

--- kernel/core/entropy.c
+++ kernel/core/entropy.c
@@ -1,30 +1,37 @@
 #include <truth/entropy.h>
+#include <truth/jitter.h>
+#include <truth/lock.h>
 
-#define Entropy_Pool_Size 512
+#define Entropy_Pool_Size 256
 
-atomic_int pool_head = ATOMIC_INT_LOCK_FREE;
-uint8_t entropy_pool[Entropy_Pool_Size];
+// FIXME: Use something more efficient than a spinlock!
+static struct lock entropy_lock = Lock_Clear;
+int Entropy_Pool_Index = 0;
+uint8_t Entropy_Pool[Entropy_Pool_Size];
 
 
-
-void entropy_pool_seed(uint8_t byte) {
-    int index = atomic_fetch_add_explicit(&pool_head, 1, memory_order_acquire);
-    if (index >= Entropy_Pool_Size) {
-        atomic_fetch_sub_explicit(&pool_head, 1, memory_order_acquire);
-        return;
-    }
-    entropy_pool[index] = byte;
+void entropy_pool_seed(uint8_t byte, uint64_t i) {
+    lock_acquire_writer(&entropy_lock);
+    Entropy_Pool_Index++;
+    Entropy_Pool[Entropy_Pool_Index] = byte;
+    lock_release_writer(&entropy_lock);
 }
 
-uint8_t entropy_pool_consume(void) {
-    int index;
-    while (true) {
-        index = atomic_fetch_sub_explicit(&pool_head, 1, memory_order_acquire);
-        if (index >= 0) {
-            break;
-        }
-        atomic_fetch_add_explicit(&pool_head, 1, memory_order_acquire);
-    }
 
-    return entropy_pool[index];
+uint8_t entropy_pool_consume(void) {
+
+    lock_acquire_writer(&entropy_lock);
+    if (Entropy_Pool_Index < 0) {
+        lock_release_writer(&entropy_lock);
+        uint64_t memory_jitter_entropy = memory_jitter_calculate();
+        for (size_t i = 0; i < sizeof(memory_jitter_entropy); ++i) {
+            entropy_pool_seed(memory_jitter_entropy & 0xff, 0xdeadbeef);
+            memory_jitter_entropy >>= 8;
+        }
+        lock_acquire_writer(&entropy_lock);
+    }
+    uint8_t value = Entropy_Pool[Entropy_Pool_Index];
+    Entropy_Pool_Index--;
+    lock_release_writer(&entropy_lock);
+    return value;
 }
