a87e1e6dc45c0b9e8427cbea170d90e3fdae3865
Ian Kronquist <iankronquist@gmail.com>
2017-07-12 17:24:14 -0400

    Move structure definitions to header file
    Share the definitions with the secondary loader

--- include/arch/x64/segments.h
+++ include/arch/x64/segments.h
@@ -12,4 +12,60 @@
 
 void tss_set_stack(void *stack);
 
+struct tss_entry {
+    uint32_t reserved0;
+    uint64_t stack0;
+    uint64_t stack1;
+    uint64_t stack2;
+    uint64_t reserved1;
+    uint64_t ist[7];
+    uint64_t reserved2;
+    uint16_t reserved3;
+    uint16_t iomap_base;
+} pack;
+
+struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+};
+
+struct gdt_register {
+    uint16_t limit;
+    uint64_t base;
+} pack;
+
+
+#define Gran_64_Bit_Mode (1 << 5)
+#define Gran_32_Bit_Mode (1 << 6)
+#define Gran_4_KB_Blocks (1 << 7)
+
+#define gdt_entry(base, limit, access, granularity) \
+    { (limit) & 0xffff, \
+      (uint16_t) ((base) >> 0  & 0xffff), \
+      (uint8_t)  ((base) >> 16 & 0xff), \
+      (access) & 0xff, \
+      ((limit) >> 16 & 0x0f) | ((granularity) & 0xf0), \
+      (uint8_t) ((base) >> 24 & 0xff), \
+    }
+
+#define gdt_entry64(base, limit, access, granularity) \
+    { (limit) & 0xffff, \
+      (uint16_t) ((base) >> 0  & 0xffff), \
+      (uint8_t)  ((base) >> 16 & 0xff), \
+      (access) & 0xff, \
+      ((limit) >> 16 & 0x0f) | ((granularity) & 0xf0), \
+      (uint8_t) ((base) >> 24 & 0xff), \
+    }, \
+    { (uint16_t) ((base) >> 32 & 0xffff), \
+      (uint16_t) ((base) >> 48 & 0xffff), \
+      0, \
+      0, \
+      0, \
+      0, \
+    } \
+
 #endif
