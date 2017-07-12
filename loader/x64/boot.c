a2a4e53e8607521447c707719367855cf409f8af
Ian Kronquist <iankronquist@gmail.com>
2017-07-12 01:14:39 -0400

    Move string functions into own file

--- loader/x64/boot.c
+++ loader/x64/boot.c
@@ -1,3 +1,4 @@
+#include <loader/string.h>
 #include <truth/boot.h>
 #include <truth/types.h>
 #include <truth/memory.h>
@@ -38,30 +39,7 @@ void boot_halt(void);
 struct boot_info *Boot_Info_Pointer = NULL;
 
 
-// FIXME: This is inefficient.
-void *memset(void *b, int c, size_t len) {
-    uint8_t *buf = b;
-    for (size_t i = 0; i < len; ++i) {
-        buf[i] = c;
-    }
-    return b;
-}
 
-size_t strlen(const char *str) {
-    const char *c;
-    for (c = str; *c != '\0'; ++c) { }
-    return c - str;
-}
-
-int strncmp(const char *s1, const char *s2, size_t n) {
-    for (size_t i = 0; i < n && *s1 != '\0' && *s2 != '\0'; ++s1, ++s2, ++i) {
-        int diff = *s1 - *s2;
-        if (diff != 0) {
-            return diff;
-        }
-    }
-    return *s1 - *s2;
-}
 
 
 struct tss_entry {
