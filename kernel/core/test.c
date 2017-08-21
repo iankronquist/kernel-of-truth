708004157299c728b304385f3f4255ffbd587f60
Ian Kronquist <iankronquist@gmail.com>
2017-08-21 08:12:14 -0700

    Remove debug messages and duplicate code

--- kernel/core/test.c
+++ kernel/core/test.c
@@ -6,10 +6,6 @@
 void test_shutdown_status(enum status status) {
     logf(Log_Debug, "Test shutting down with status %s (%d)\n", status_message(status), status);
     write_port(status, TEST_RESULT_PORT_NUMBER);
-	//__asm__("movb $0x0, %al; outb %al, $0xf4");
-	__asm__("movb $0xf4, %al; outb %al, $0x0");
-	__asm__("movb $0x0, %al; outb %al, $0xf4");
-	halt();
-    //write_port(2, TEST_RESULT_PORT_NUMBER);
+    halt();
     assert(Not_Reached);
 }
