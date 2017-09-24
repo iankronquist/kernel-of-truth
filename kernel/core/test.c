7508c0b8948668b45c38cfc3dd4e2a9c4709dd3e
Ian Kronquist <iankronquist@gmail.com>
2017-09-24 13:41:02 -0700

    Exit with a code in debug builds

--- kernel/core/test.c
+++ kernel/core/test.c
@@ -1,11 +1,9 @@
 #include <arch/x64/port.h>
 #include <truth/panic.h>
 
-#define TEST_RESULT_PORT_NUMBER 0xf4
+#define Test_Result_Port_Number 0xf4
 
 void test_shutdown_status(enum status status) {
     logf(Log_Debug, "Test shutting down with status %s (%d)\n", status_message(status), status);
-    write_port(status, TEST_RESULT_PORT_NUMBER);
-    halt();
-    assert(Not_Reached);
+    write_port(status, Test_Result_Port_Number);
 }
