14379785335cc61cbd6c42fd277dc8883839e671
Ian Kronquist <iankronquist@gmail.com>
2018-04-29 15:25:58 -0700

    Rename kernel modules from *.ko to *.mod

---
+++ modules/ata/ide.d
@@ -0,0 +1,33 @@
+ide.o: ide.c ../../include/arch/x64/port.h ../../include/truth/types.h \
+ ../../include/arch/x64/cpu.h ../../include/truth/interrupts.h \
+ ../../include/truth/drive.h ../../include/truth/file.h \
+ ../../include/truth/object.h ../../include/truth/lock.h \
+ ../../include/truth/scheduler.h ../../include/truth/process.h \
+ ../../include/truth/log.h ../../include/truth/panic.h \
+ ../../include/truth/cpu.h
+
+../../include/arch/x64/port.h:
+
+../../include/truth/types.h:
+
+../../include/arch/x64/cpu.h:
+
+../../include/truth/interrupts.h:
+
+../../include/truth/drive.h:
+
+../../include/truth/file.h:
+
+../../include/truth/object.h:
+
+../../include/truth/lock.h:
+
+../../include/truth/scheduler.h:
+
+../../include/truth/process.h:
+
+../../include/truth/log.h:
+
+../../include/truth/panic.h:
+
+../../include/truth/cpu.h:
