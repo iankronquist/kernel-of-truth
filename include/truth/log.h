c3713bdb9e32566768222582a496aaa6e81e71f5
Ian Kronquist <iankronquist@gmail.com>
2018-04-29 15:13:55 -0700

    Log hexdump function

--- include/truth/log.h
+++ include/truth/log.h
@@ -18,3 +18,5 @@ void log_set_level(enum log_level level);
 void log(enum log_level level, const char *message);
 
 void logf(enum log_level level, const char *message, ...) check_format(2, 3);
+
+void log_hexdump(void *memory, size_t bytes);
